4ac5727b6c0f75ed1d2b7e5ee5b89c17b1c72942
Samuel G <gonzalezsam451@gmail.com>
2026-04-14 10:51:02 -0500

    prueba

--- password_manager.py
+++ password_manager.py
@@ -4,7 +4,8 @@ from caesar import caesar_encrypt
 
 
 def encrypt_single_pass(filename: str) -> None:
-    import csv
+    """TODO: Parte 1."""
+import csv
 from caesar import caesar_encrypt
 
 def encrypt_single_pass(filename: str) -> None:
@@ -18,12 +19,21 @@ def encrypt_single_pass(filename: str) -> None:
 
 def encrypt_passwords_in_file(filename: str) -> None:
     """TODO: Parte 2."""
-    pass
+def encrypt_passwords_in_file(filename: str) -> None:
+    with open(filename, "r") as f:
+        reader = csv.reader(f)
+        rows = [row for row in reader if row]
+    for i in range(1, len(rows)):
+        rows[i][2] = caesar_encrypt(rows[i][2])
+    with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(rows)
+
 
 
 def change_password(filename: str, website: str, password: str) -> bool:
     """TODO: Parte 3."""
-    pass
+    
 
 
 def add_login(filename: str, website_name: str, username: str, password: str) -> None:
